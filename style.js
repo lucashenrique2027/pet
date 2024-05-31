7c64c05337402ea2ac73a26a28f86e7f01333abb
celim <marceloluiz2002@gmail.com>
2024-05-31 20:56:41 -0300

    git  commit "sitePET"

---
+++ style.js
@@ -0,0 +1,71 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
+
+
+
+  let slideIndex = 0;
+
+  function showSlides() {
+    const slides = document.querySelectorAll('.carousel-content img');
+    if (slideIndex >= slides.length) {
+      slideIndex = 0;
+    } else if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+    slides.forEach(slide => slide.style.transform = `translateX(-${slideIndex * 100}%)`);
+  }
+
+  function prevSlide() {
+    slideIndex--;
+    showSlides();
+  }
+
+  function nextSlide() {
+    slideIndex++;
+    showSlides();
+  }
+
+  showSlides();
+
